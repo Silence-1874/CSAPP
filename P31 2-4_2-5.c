91556e53b72044148f13cd88b53778d6dea12684
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-05-03 15:54:54 +0800

    Update P31 2-4_2-5.c

--- P31 2-4_2-5.c
+++ P31 2-4_2-5.c
@@ -5,7 +5,7 @@ typedef unsigned char *byte_pointer;
 void show_bytes(byte_pointer start, size_t len) {
     size_t i;
     for (i = 0; i < len; i++)
-	printf("%.2x", start[i]); 
+        printf("%.2x", start[i]); 
     printf("\n");
 }
 
