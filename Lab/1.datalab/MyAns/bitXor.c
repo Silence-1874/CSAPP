b44b46a43905baa2606296768431df6f1ebad884
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-08 15:29:59 +0800

    Create bitXor.c

---
+++ Lab/1.datalab/MyAns/bitXor.c
@@ -0,0 +1,10 @@
+/* 
+ * bitXor - x^y using only ~ and & 
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 1
+ */
+int bitXor(int x, int y) {
+    return ~(~(x & ~y) & ~(~x & y));
+}
