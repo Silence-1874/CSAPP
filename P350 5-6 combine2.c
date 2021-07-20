3081e3a9db12eaa2094ca3042f8c3cf2cf99b72e
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-20 21:56:22 +0800

    Merge branch 'main' of https://github.com/Silence-Zoe/CSAPP into main

---
+++ P350 5-6 combine2.c
@@ -0,0 +1,12 @@
+void combine2(vec_ptr v, data_t *dest) 
+{
+    long i;
+    long length = vec_length(v);
+
+    *dest = IDENT;
+    for (i = 0; i < length; i++) {
+        data_t val;
+        get_vec_element(v, i, &val);
+        *dest = *dest OP val;
+    }
+}
