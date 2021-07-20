bac6f796716723d6189db87e7f3605eb7948e43a
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-20 22:05:14 +0800

    Create P355 5-10 combine4.c

---
+++ P355 5-10 combine4.c
@@ -0,0 +1,12 @@
+void combine4(vec_ptr v, data_t *dest)
+{
+    long i;
+    long length = vec_length(v);
+    data_t *data = get_vec_start(v);
+    data_t acc = IDENT;
+
+    for (i = 0; i < length; i++) {
+        acc = acc OP data[i];
+    }
+    *dest = acc;
+}
